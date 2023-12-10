8bfa2fddff4fd6243ce2a9b8303e1972a12de5ba
elluy-gabriel-p <elluygabrielpanambe@gmail.com>
2023-12-10 14:25:26 +0700

    Database Hosting

--- public_html/index.php
+++ public_html/index.php
@@ -33,8 +33,6 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 
 require __DIR__.'/../vendor/autoload.php';
 
-
-
 /*
 |--------------------------------------------------------------------------
 | Run The Application
